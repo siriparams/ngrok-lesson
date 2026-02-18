29562c34682c0ec0edfc8265e764ddb7e50af5ad
siriparams <siriparamathma06@gmail.com>
2026-02-18 10:10:03 +0530

    111

---
+++ db.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Vercel provides this via environment variables
+SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRES_URL").replace("postgres://", "postgresql://")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
+from sqlalchemy import Column, Integer, String
 from db import Base
-from sqlalchemy import Column, Integer,String
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
     email = Column(String, unique=True, index=True)
-    password = Column(String)
